8740041ee0602f6f82a6e838bdde9ee3fee83974
Patriks <p.kulinskis@gmail.com>
2024-12-22 18:57:58 +0200

    Gui update 2

--- src/Kulinskis/Gui.java
+++ src/Kulinskis/Gui.java
@@ -4,10 +4,10 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Gui {
-    static void createGui() {
+     static void createGui() {
         JFrame logs = new JFrame("Automašīnas");
         logs.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        logs.setSize(950, 350);
+        logs.setSize(850, 350);
         ImageIcon ikona = new ImageIcon("src/car.png");
         logs.setIconImage(ikona.getImage());
         logs.setResizable(false);
@@ -23,73 +23,58 @@ public class Gui {
         JPanel pogasPanel3 = new JPanel();
         JPanel panel = new JPanel(new FlowLayout());
 
-        pogasPanel1.setBorder(BorderFactory.createTitledBorder("LEFT"));
-        pogasPanel2.setBorder(BorderFactory.createTitledBorder("CENTER"));
-        pogasPanel3.setBorder(BorderFactory.createTitledBorder("RIGHT"));
+        pogasPanel1.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.lightGray, 2),"Strādāt ar failu"));
+        pogasPanel2.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.lightGray, 2),"Apskatīt vai dzēst objektus"));
+        pogasPanel3.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.lightGray, 2),"Izveidot automašīnas objektu"));
 
-        BoxLayout layout1 = new BoxLayout(pogasPanel1, BoxLayout.Y_AXIS);
-        BoxLayout layout2 = new BoxLayout(pogasPanel2, BoxLayout.Y_AXIS);
-        BoxLayout layout3 = new BoxLayout(pogasPanel3, BoxLayout.Y_AXIS);
-        pogasPanel1.setLayout(layout1);
-        pogasPanel2.setLayout(layout2);
-        pogasPanel3.setLayout(layout3);
+        pogasPanel1.setLayout(new GridLayout(2, 1,0 , 6));
+        pogasPanel2.setLayout(new GridLayout(2, 1,0 , 6));
+        pogasPanel3.setLayout(new GridLayout(2, 1,0 , 6));
 
-        JButton poga1 = new JButton("Izveidot automašīnas objektu ar esošiem objektiem");
+        JButton poga1 = new JButton("Saglabāt automašīnas objektu failā");
         poga1.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        poga1.setMargin(new Insets(6, 22, 6, 22));
+        poga1.setMargin(new Insets(6, 10, 6, 10));
         poga1.setFocusPainted(false);
-        poga1.addActionListener(e -> DarbsArObjektiem.izveidotObjektu());
+        poga1.addActionListener(e -> DarbsArFailu.saglabatFaila());
 
-        JButton poga2 = new JButton("Izveidot automašīnas objektu no jauna");
+        JButton poga2 = new JButton("Apskatīt faila saturu");
         poga2.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        poga2.setMargin(new Insets(6, 22, 6, 22));
+        poga2.setMargin(new Insets(6, 10, 6, 10));
         poga2.setFocusPainted(false);
-        poga1.addActionListener(e -> DarbsArObjektiem.izveidotObjektu());
+        poga2.addActionListener(e -> DarbsArFailu.apskatitFailu());
 
-        poga1.setAlignmentX(Component.LEFT_ALIGNMENT);
-        poga2.setAlignmentX(Component.LEFT_ALIGNMENT);
-        pogasPanel1.add(Box.createRigidArea(new Dimension(8, 8)));
         pogasPanel1.add(poga1);
-        pogasPanel1.add(Box.createRigidArea(new Dimension(8, 8)));
         pogasPanel1.add(poga2);
 
-        JButton poga3 = new JButton("Saglabāt automašīnas objektu failā");
+        JButton poga3 = new JButton("Apskatīt izveidotos objektus");
         poga3.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        poga3.setMargin(new Insets(6, 22, 6, 22));
+        poga3.setMargin(new Insets(6, 10, 6, 10));
         poga3.setFocusPainted(false);
-        poga3.addActionListener(e -> DarbsArFailu.saglabatFaila());
+        poga3.addActionListener(e -> DarbsArObjektiem.apskatitObjektus());
 
-        JButton poga4 = new JButton("Saglabāt automašīnas objektu failā");
+        JButton poga4 = new JButton("Dzēst izveidotos objektus");
         poga4.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        poga4.setMargin(new Insets(6, 22, 6, 22));
+        poga4.setMargin(new Insets(6, 10, 6, 10));
         poga4.setFocusPainted(false);
-        poga4.addActionListener(e -> DarbsArFailu.saglabatFaila());
+        poga4.addActionListener(e -> DarbsArObjektiem.dzestObjektus());
 
-        poga3.setAlignmentX(Component.CENTER_ALIGNMENT);
-        poga4.setAlignmentX(Component.CENTER_ALIGNMENT);
-        pogasPanel2.add(Box.createRigidArea(new Dimension(8, 8)));
         pogasPanel2.add(poga3);
-        pogasPanel2.add(Box.createRigidArea(new Dimension(8, 8)));
         pogasPanel2.add(poga4);
 
-        JButton poga5 = new JButton("Saglabāt automašīnas objektu failā");
+        JButton poga5 = new JButton("Izveidot automašīnas objektu ar esošām detļām");
         poga5.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        poga5.setMargin(new Insets(6, 22, 6, 22));
+        poga5.setMargin(new Insets(6, 10, 6, 10));
         poga5.setFocusPainted(false);
         poga5.addActionListener(e -> DarbsArFailu.saglabatFaila());
 
-        JButton poga6 = new JButton("Saglabāt automašīnas objektu failā");
+        JButton poga6 = new JButton("Izveidot automašīnas objektu no jauna");
         poga6.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        poga6.setMargin(new Insets(6, 22, 6, 22));
+        poga6.setMargin(new Insets(6, 10, 6, 10));
         poga6.setFocusPainted(false);
-        poga6.setMargin(new Insets(6, 22, 6, 22));
+        poga6.setMargin(new Insets(6, 10, 6, 10));
         poga6.addActionListener(e -> DarbsArFailu.saglabatFaila());
 
-        poga5.setAlignmentX(Component.RIGHT_ALIGNMENT);
-        poga6.setAlignmentX(Component.RIGHT_ALIGNMENT);
-        pogasPanel3.add(Box.createRigidArea(new Dimension(8, 8)));
         pogasPanel3.add(poga5);
-        pogasPanel3.add(Box.createRigidArea(new Dimension(8, 8)));
         pogasPanel3.add(poga6);
 
         panel.add(pogasPanel1);
@@ -97,7 +82,6 @@ public class Gui {
         panel.add(pogasPanel3);
         logs.add(panel, BorderLayout.CENTER);
 
-       // logs.pack();
         logs.setVisible(true);
     }
 }
